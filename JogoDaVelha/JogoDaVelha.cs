9e39a0197568d9d173e2bd27f7cebe0f94dc3f06
Breno da Luz Maciel <maciel_lpnm@hotmail.com>
2024-02-28 11:42:58 -0300

    Criando o tabuleiro e peças

--- JogoDaVelha/JogoDaVelha.cs
+++ JogoDaVelha/JogoDaVelha.cs
@@ -1,13 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace JogoDaVelha
 {
-    internal class JogoDaVelha
+    class JogoDaVelha
     {
-        
+        private char[] PosicaoDasPecas;
+        private bool FimDeJogo;
+        private char vez;
+
+        public JogoDaVelha()
+        {
+            FimDeJogo = false;
+            PosicaoDasPecas = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+            vez = 'x';
+        }
+
+        public void Iniciar()
+        {
+            RenderizarJogo();
+            //LerJogada();
+            //RenderizarJogo();
+            //HouveGanhador();
+            MudarVez();
+
+
+        }
+
+        private void MudarVez()
+        {
+            vez = vez == 'x' ? 'o' : 'x';
+        }
+
+        private void NovaJogada()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void HouveGanhador()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LerJogada()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RenderizarJogo()
+        {
+            Console.Clear();
+            Console.WriteLine(ObterTabela());
+        }
+
+        private string ObterTabela()
+        {
+            return $"__{PosicaoDasPecas[0]}__|__{PosicaoDasPecas[1]}__|__{PosicaoDasPecas[2]}__ \n" +
+                   $"__{PosicaoDasPecas[3]}__|__{PosicaoDasPecas[4]}__|__{PosicaoDasPecas[5]}__ \n" +
+                   $"  {PosicaoDasPecas[6]}  |  {PosicaoDasPecas[7]}  |  {PosicaoDasPecas[8]}  \n\n";
+        }
     }
 }
--- JogoDaVelha/Program.cs
+++ JogoDaVelha/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace JogoDaVelha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new JogoDaVelha().Iniciar();
+        }
+    }
+}
